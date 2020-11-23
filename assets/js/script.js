7cb935f44761cdad23ac143cea84b158a7b34d26
Flavie-LHP5 <name>
2020-11-23 15:18:53 +0100

    vghfghfh

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+const sr = ScrollReveal({
+    duration: 1000,
+    reset : true
+});
+
+sr.reveal('h1', {
+});
+
+sr.reveal ('.lead', {
+    delay: 600
+});
+
+sr.reveal ('#card1', {
+    delay : 800
+},50 );
+
+sr.reveal ('#card2', {
+    delay : 1000
+},50 );
+
+sr.reveal ('#card3', {
+    delay : 1200
+},50 );
+
+sr.reveal ('#card4', {
+    delay : 1500
+},50 );
+
+sr.reveal ('#card5', {
+    delay : 1800
+},50 );
+
+sr.reveal ('#card6', {
+    delay : 2100
+},50 );
